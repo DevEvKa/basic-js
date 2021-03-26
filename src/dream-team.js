5eeab140d9fa7cd5580e538321b7911324183948
Evgeniya Karabanova <karabanovaep@yandex.ru>
2021-03-26 13:06:32 +0300

    feat: add completed dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+  if ( !Array.isArray(members) || members.length==0) {
+    return false
+  } else {
+    for (let i=0; i<members.length; i++) {
+      if(typeof members[i] === "string" && members[i]!== ' ') {
+        dreamTeam.push(members[i].trimLeft()[0].toUpperCase())
+      }
+    }
+  }
+  return dreamTeam.sort().join('')
 };
